dc9d4c825447144b50cc3a8e5548f0476bd4b472
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-12-23 18:31:39 +0530

    no conflict

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>blg-laravel9-app</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in turpis varius, semper justo in, pulvinar purus. Quisque malesuada ullamcorper libero, placerat posuere nibh sodales id. Mauris vel ligula bibendum, porttitor elit nec, vehicula tellus. Pellentesque tincidunt lectus id purus eleifend sollicitudin. Nam venenatis pretium turpis, quis tempor lectus. Quisque auctor mauris at lectus blandit, in vulputate mi blandit. Nam molestie neque lacus, id commodo mi euismod in. Aenean nisi elit, ultricies et vestibulum eget, tristique id arcu. Mauris euismod bibendum purus ac semper. Quisque eu varius lectus, sed volutpat risus. Nunc gravida accumsan enim nec scelerisque. Integer a tempor dolor. 
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
+</html>
