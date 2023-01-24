70917f73a1de7fbd843734993ccd326065451f9e
LAMP-Dev-In <lamp.developer.in@gmail.com>
2023-01-24 11:37:40 +0530

    post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,55 +2,10 @@
 
 namespace App\Models;
 
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-
-    public function __construct
-    (
-        public string $title,
-        public string $excerpt,
-        public int $date,
-        public string $body,
-        public string $slug
-
-    ){}
-
-    public static function all()
-    {
-
-       return cache()->remember('posts.all', 1200, function(){
-
-            return collect(File::files(resource_path("posts/")))
-             ->map(fn($file) => YamlFrontMatter::parseFile($file))
-             ->map(fn($document) => new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-                ))
-            ->sortByDesc('date');
-
-        });
-        
-    }
-
-    public static function find($slug)
-    {        
-        return static::all()->firstWhere('slug', $slug);        
-    }
-
-    public static function findOrFail($slug)
-    {        
-        $post = static::find($slug);  
-               
-        if(!$post) throw new ModelNotFoundException();
-
-        return $post;
-    }
-
+    use HasFactory;
 }
