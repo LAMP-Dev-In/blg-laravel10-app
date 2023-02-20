0085b69a7516b8c984b690e114df8de6007ae2ea
LAMP-Dev-In <lamp.developer.in@gmail.com>
2023-02-20 12:32:18 +0530

    posts by author

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return view('posts',[
-        'posts' => Post::latest()->with('category')->get()
+        'posts' => Post::latest()->with(['category', 'author'])->get()
     ]);
 
 });
@@ -37,6 +38,14 @@ Route::get('categories/{category:slug}', function(Category $category){
 
     return view('posts', [
         'posts' => $category->posts
+    ]);
+
+});
+
+Route::get('authors/{author}', function(User $author){
+
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 
 });
