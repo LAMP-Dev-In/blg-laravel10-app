23dcf338aae9c154a9a334e4a6a46270d7c378f5
LAMP-Dev-In <lamp.developer.in@gmail.com>
2023-03-06 10:54:34 +0530

    published day ago + updated to Laravel 10

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,9 +2,9 @@
     @include ('_post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-featured-card />
+            <x-post-featured-card :post="$posts[0]" />
             
-        <div class="lg:grid lg:grid-cols-2">
+<!--    <div class="lg:grid lg:grid-cols-2">
             <x-post-card /> 
             <x-post-card /> 
         </div>
@@ -13,9 +13,11 @@
             <x-post-card /> 
             <x-post-card /> 
             <x-post-card />     
-        </div>
+        </div> -->
 
-    </main>    
+
+    </main>  
+
 <!--     @foreach($posts as $post)
         <article>
             
