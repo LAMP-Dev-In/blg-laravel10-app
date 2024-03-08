b377c47c87c122670065717628fd379ce594328b
LAMP-Dev-In <lamp.developer.in@gmail.com>
2024-03-08 13:45:01 +0530

    convert blog post page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,11 +2,11 @@
     @include ('_post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        @if ($posts->count())
-            <x-posts-grid :posts="$posts" />
-        @else
-            <p class="text-center">No post yet. Please check back later.</p>
-        @endif
+            @if ($posts->count())
+                <x-posts-grid :posts="$posts" />
+            @else
+                <p class="text-center">No post yet. Please check back later.</p>
+            @endif
 
     </main>  
 
