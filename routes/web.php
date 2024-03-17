e86532e70e8dda5c95f0fc9f1855e2fa76ab0c82
LAMP-Dev-In <lamp.developer.in@gmail.com>
2024-03-17 22:01:09 +0530

    Category dropdown into blade component

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -24,7 +22,6 @@ Route::get('authors/{author:username}', function(User $author){
 
     return view('posts', [
         'posts' => $author->posts,
-        'categories' => Category::all()
     ]);
 
 });
