3a9c9e47d39bdb7f166cb6963f9de1fd93e06c91
LAMP-Dev-In <lamp.developer.in@gmail.com>
2024-03-22 14:35:47 +0530

    login page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,8 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionController extends Controller
 {
+    /**
+     * login actions
+     */
+    public function create()
+    {
+        return view('session.create');
+    }
+
+
+    public function store()
+    {
+        $attributes =  request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($attributes)){
+
+            //avoid session fixation
+            session()->regenerate();
+
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        // auth failed.
+        throw ValidationException::withMessages([
+            'email' => 'Your credientials could not be varified.'
+        ]);
+
+    }
+
+    /**
+     * logout actions
+     */
     public function destroy()
     {
         auth()->logout();
