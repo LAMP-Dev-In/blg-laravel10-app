7fd24483e80f38e03e5b5cc740bd1d4dc494e679
LAMP-Dev-In <lamp.developer.in@gmail.com>
2024-04-02 12:34:47 +0530

    comments form activated

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,9 +50,11 @@
                     </div>
                 </div>
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    @auth 
+
                     < x-panel>
                     
-                    <form action="#" method="post">
+                    <form method="POST" action="/posts/{{ $post->slug }}/comments">
                         @csrf
 
                         <header class="flex items-center">
@@ -63,7 +65,10 @@
 
 
                         <div class="mt-6">
-                            <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Quick, thing of something to say!"></textarea>                                
+                            <textarea 
+                            name="body" 
+                            class="w-full text-sm focus:outline-none focus:ring" 
+                            rows="5" placeholder="Quick, thing of something to say!"></textarea>                                
                         </div>
 
                         <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
@@ -73,7 +78,12 @@
                     </form>
                     
                     </x-panel>
-                    
+                    @else
+                        <p class="font-semibold">
+                            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">log in</a> to leave a comment.
+                        </p>
+
+                    @endauth
                 
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
