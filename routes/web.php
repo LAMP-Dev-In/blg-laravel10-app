4ba25819973db4c299a6c71e0a08f266c48b395c
LAMP-Dev-In <lamp.developer.in@gmail.com>
2024-04-04 23:34:56 +0530

    third party API tinkering

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,24 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('ping', function() {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => config('services.mailchimp.server'),
+    ]);
+
+    $response = $mailchimp->lists->addListMember("1c3d358b4b", [
+        "email_address" => "gautam.php@gmail.com",
+        "status" => "subscribed",
+    ]);
+    
+    
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
