c0cf36247f2b5a622bbb5f012960889bbd00268e
LAMP-Dev-In <lamp.developer.in@gmail.com>
2024-04-08 18:04:51 +0530

    configure Newsletter service provider

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -11,7 +11,7 @@ class NewsletterController extends Controller
 {
     public function __invoke(Newsletter $newsletter)
     {
-        
+
         request()->validate(['email' => 'required|email']);
 
         try {
